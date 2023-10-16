d56d1d464b28e1092430a05927740f89eadc11c3
pacsIsStrange <123695766+pacsIsStrange@users.noreply.github.com>
2023-10-16 20:18:04 -0300

    Update main.c

--- main.c
+++ main.c
@@ -26,14 +26,16 @@ int main() {
                "cadastrar um novo.\n");
     } else if (opcao == 2) {
       codAux = apagaCliente(&lc);
-      if (codAux == 1)
+      if (codAux == 1) {
         printf("ERRO: nao existem tarefas para deletar\n");
-      else if (codAux == 2)
+      } else if (codAux == 2) {
         printf("ERRO: posicao da tarefa invalida\n");
+      }
     } else if (opcao == 3) {
       codAux = listarClientes(&lc);
-      if (codAux == 1)
+      if (codAux == 1) {
         printf("erro ao listar tarefas: nao existem tarefas para listar\n");
+      }
     } else if (opcao == 4) {
       long long int cpf;
       float valor = 0;
@@ -44,13 +46,27 @@ int main() {
       scanf("%s", &senha);
       printf("Digite o valor que sera debitado: \n");
       scanf("%f", &valor);
-      printf("AAA");
       codAux = debito(&lc, cpf, &senha, valor);
-      printf("BBB");
-      if (codAux == 1)
+      if (codAux == 0) {
+        printf("Operacao realizada com sucesso\n");
+      }
+      if (codAux == 1) {
         printf("ERRO: CPF nao encontrado\n");
-    } else if (codAux == 2) {
-      printf("Erro desconhecido\n");
+      }
+    } else if (opcao == 5) {
+      long long int cpf;
+      float valor = 0;
+      printf("Digite o CPF do cliente: \n");
+      scanf("%lld", &cpf);
+      printf("Digite o valor que sera depositado: \n");
+      scanf("%f", &valor);
+      codAux = deposito(&lc, cpf, valor);
+      if (codAux == 0) {
+        printf("Operacao realizada com sucesso");
+      }
+      if (codAux == 1) {
+        printf("ERRO: CPF nao encontrado\n");
+      }
     }
   } while (opcao != 0);
 
