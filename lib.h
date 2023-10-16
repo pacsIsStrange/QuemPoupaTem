b4a34c980a3b3dd492b5e96a85946cf8524565bb
laaurap <146039258+laaurap@users.noreply.github.com>
2023-10-16 20:31:27 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,21 +1,50 @@
-#ifndef QUEMPOUPATEM_LIB_H
-#define QUEMPOUPATEM_LIB_H
+#define TOTAL_CLIENTES 1000
 
- struct Clientes{
-    char nome[100];
-    char cpf[11];
-    int tipo;
-    float saldo;
-    char senha[20];
-};
-int tamanho(struct Clientes *lc);
-int novoCliente();
-int apagaCliente();
-int listarClientes();
-int debito();
-int deposito();
-int extrato();
-int transferencia();
-int salvar();
+typedef struct {
+  char nome[100];
+  long long int cpf;
+  int tipo;
+  float saldo;
+  char senha[20];
+} Cliente;
 
-#endif //QUEMPOUPATEM_LIB_H
+typedef struct {
+  Cliente clientes[TOTAL_CLIENTES];
+  int qtd;
+} ListaDeClientes;
+
+typedef struct {
+  char descricao[20];
+  int valor;
+  double taxa;
+  long long int cpf;
+} Operacao;
+
+typedef struct {
+  Operacao operacoes[100000];
+  int qtd;
+} Extrato;
+
+typedef struct {
+
+} operacao;
+
+int novoCliente(ListaDeClientes *lc);
+int apagaCliente(ListaDeClientes *lc);
+int listarClientes(ListaDeClientes *lc);
+int debito(ListaDeClientes *lc, long long int cpf, char *senha, float valor);
+int deposito(ListaDeClientes *lc, long long int cpf, float valor);
+int transferencia(ListaDeClientes *lc, long long int cpfOrigem,
+                  const char *senha, long long int cpfDestino, float valor);
+int carregar(ListaDeClientes *lc, char *strArquivo);
+int salvar(ListaDeClientes *lc, char *strArquivo);
+void copiaString(char string1[], char string2[]);
+int tamanho(char string[]);
+float retornaTaxa(int tipo);
+int carregarExtrato(Extrato *ext, char *strArquivo);
+int salvarExtrato(Extrato *ext, char *strArquivo);
+int buscarExtrato(Extrato *ext, long long int cpf, char *senha);
+int registrarExtrato(Extrato *ext, char descricao[20], int valor,
+double taxa, long long int cpf, char *strArquivo);
+
+void exibeMenu();
