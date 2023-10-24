e8cc103f40678da300a220c8688b93a09abffe11
pacsIsStrange <123695766+pacsIsStrange@users.noreply.github.com>
2023-10-24 20:41:11 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,13 +2,12 @@
 
 typedef struct {
   char descricao[20];
-  double valor;
-  double taxa;
-  long long int cpf;
+  float valor;
+  float taxa;
 } Operacao;
 
 typedef struct {
-  Operacao operacoes[100000];
+  Operacao operacoes[100];
   int qtd;
 } Extrato;
 
@@ -18,24 +17,23 @@ typedef struct {
   int tipo;
   float saldo;
   char senha[20];
+  Extrato ext;
 } Cliente;
 
 typedef struct {
-  Cliente clientes[TOTAL_CLIENTES];
+  Cliente clientes[10];
   int qtd;
 } ListaDeClientes;
 
 int novoCliente(ListaDeClientes *lc);
 int apagaCliente(ListaDeClientes *lc);
 int listarClientes(ListaDeClientes *lc);
-int debito(Extrato *ext, ListaDeClientes *lc, long long int cpf, float valor);
-int deposito(Extrato *ext, ListaDeClientes *lc, long long int cpf, float valor);
-int escreveExtrato(Extrato ext, ListaDeClientes *lc, long long int cpf, char *senha);
-int transferencia(Extrato *ext, ListaDeClientes *lc, long long int cpfOrigem, long long int cpfDestino, float valor, char *senha);
+int debito(ListaDeClientes *lc, long long int cpf, float valor);
+int deposito(ListaDeClientes *lc, long long int cpf, float valor);
+int escreveExtrato(ListaDeClientes *lc, long long int cpf);
+int transferencia(ListaDeClientes *lc, long long int cpfOrigem, long long int cpfDestino, float valor, char *senha);
 int carregarClientes(ListaDeClientes *lc, char *strArquivo);
 int salvarClientes(ListaDeClientes *lc, char *strArquivo);
-int carregarExtrato(Extrato *ext, char *strArquivo);
-int salvarExtrato(Extrato *ext, char *strArquivo);
 void copiaString(char string1[], char string2[]);
 int tamanho(char string[]);
 float retornaTaxa(int tipo);
